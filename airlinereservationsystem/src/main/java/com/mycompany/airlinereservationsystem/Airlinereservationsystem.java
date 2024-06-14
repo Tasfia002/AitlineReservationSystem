cec2c7de4d9bad722bddbb5ea7b36dbb95fa023d
Syeda Tasfia Tabassum <168075019+Tasfia002@users.noreply.github.com>
2024-06-14 18:28:30 +0600

    Add files via upload

---
+++ airlinereservationsystem/src/main/java/com/mycompany/airlinereservationsystem/Airlinereservationsystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airlinereservationsystem;
+
+/**
+ *
+ * @author DELL
+ */
+public class Airlinereservationsystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
